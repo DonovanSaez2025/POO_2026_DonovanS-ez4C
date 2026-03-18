ef74422bc5bbb745533f22f3ec81f37b965396f4
DonovanSaez2025 <donovansaez@liceovvh.cl>
2026-03-18 15:09:37 -0300

    g
    g

--- 04_funciones_JS/static/js/script.js
+++ 04_funciones_JS/static/js/script.js
@@ -1,4 +1,5 @@
 console.log("Conectado a JS...");
+const INVALIDO = "El nombre no puede estar vacío.";
 
 /* Ejercicio 1: Lista de Asistencia (push)
 Contexto: Eres el profesor y estás pasando la lista. Cada alumno que dice "presente" debe ser anotado al final del registro.
@@ -9,16 +10,54 @@ Contexto: Eres el profesor y estás pasando la lista. Cada alumno que dice "pres
 - Modifica el textContent del párrafo para mostrar: "Alumnos presentes: " seguido del arreglo.
 - Limpia el input vaciando su .value.*/
 let asistencia = [];
+function agregarLista(nombre) {
+    asistencia.push(nombre);
+    return asistencia.join(", ");
+};
+
 function registrarAlumno() {
-    let input = document.getElementById("input1").value;
-    let resultadoContenedor1 = document.getElementById("resultadoContenedor1");
-
-    if (input == "") {
-        alert("El nombre no puede estar vacío.")
+    const contenedor = document.getElementById("resultadoContenedor1");
+    const resultado1 = document.getElementById("resultado1");
+    const input = document.getElementById("input1");
+    if (input.value == "") {
+        alert(INVALIDO);
     } else {
-        asistencia.push(input);
-        alert("Alumno enlistado.");
+        let nombre = input.value;
+        let resultado = agregarLista(nombre);
+        resultado1.textContent = resultado;
+        input.value = "";
+        contenedor.classList.remove("escondido");
+        alert("Alumno enlistado correctamente.");
     };
+};
 
-    resultadoContenedor1.classList.remove("desaparece");
-}
+/* Ejercicio 2: Fila de Urgencias Médicas (unshift e if)
+Contexto: En un hospital, los pacientes graves no van al final de la fila, pasan directamente al primer lugar de atención.
+- Crea un arreglo: let pacientes = ["Carlos", "María", "Diego"];
+- Función Principal: Crea ingresarUrgencia().
+- Captura el nombre del paciente desde el input.
+- Usa un if para revisar si el input NO está vacío (!== "").
+- Si escribieron un nombre, usa .unshift() para agregarlo al inicio del arreglo pacientes.
+- Muestra en el textContent del párrafo: "Próximos a atender: " seguido del arreglo.
+- Limpia el input.*/
+let pacientes = ["Carlos", "María", "Diego"];
+function agregarUrgencia(nombre) {
+    pacientes.push(nombre);
+    return pacientes.join(", ");
+};
+
+function ingresarUrgencia() {
+    const contenedor = document.getElementById("resultadoContenedor2");
+    const resultado1 = document.getElementById("resultado2");
+    const input = document.getElementById("input2");
+    if (input.value == "") {
+        alert(INVALIDO);
+    } else {
+        let nombre = input.value;
+        let resultado = agregarUrgencia(nombre);
+        resultado1.textContent = resultado;
+        input.value = "";
+        contenedor.classList.remove("escondido");
+        alert("Alumno enlistado correctamente.");
+    };
+};
